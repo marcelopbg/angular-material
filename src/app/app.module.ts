e5b4bc798ad3c342c6a2eb61566a23d23bbc7eef
root <marcelo.guimaraes@agoraentert.com.br>
2018-12-31 11:30:40 -0200

    responsive sidebar adjust

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,19 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidenavAutosizeComponent } from './sidenav-autosize/sidenav-autosize.component';
+/* import { SidenavAutosizeComponent } from './sidenav-autosize/sidenav-autosize.component';
+ */import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidenavAutosizeComponent
-  ],
+/*     SidenavAutosizeComponent
+ */  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SidenavAutosizeComponent
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
